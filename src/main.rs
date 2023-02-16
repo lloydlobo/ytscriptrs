e01de5961626775317ac11211aba809cdaace4e4
Lloyd Lobo <lloydlobo@gmail.com>
2023-02-16 18:03:48 +0530

    perf: use buffer to collect para
    first use a buffer string to push '&str' to,
    then if buffer has some, push it to the Vec
    Note, had used Vec with capacity
       but using default vec new

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,10 @@ use std::{fs::File, process::Command};
 
 use anyhow::{anyhow, Result};
 use once_cell::sync::Lazy;
-use rayon::{iter::ParallelIterator, prelude::ParallelBridge};
+use rayon::{
+    iter::ParallelIterator,
+    prelude::{ParallelBridge},
+};
 use xml::reader::{EventReader, XmlEvent};
 
 static ARGS: Lazy<[&'static str; 6]> = Lazy::new(|| {
@@ -60,37 +63,58 @@ fn find_subtitle_filename(output: &str) -> Result<String> {
 
 //////////////////////////////////////////////////////////////////////////////////
 
+// Avoid unnecessary allocations: In the extract_subtitles_xml function, you are currently pushing
+// each subtitle to a Vec<String>. However, you could avoid allocating a new string for each
+// subtitle by using a String buffer and appending the subtitles to it. You can also preallocate the
+// Vec with an initial capacity to avoid reallocations as you add more elements. For example:
+// let mut subtitles: Vec<String> = vec![];
+// for event in parser.into_iter() {
+// match event {
+// Ok(XmlEvent::Characters(s)) => {
+// if !s.trim().is_empty() {
+// subtitles.push(s); } }
 fn extract_subtitles_xml(xml_file: &str) -> Result<Vec<String>> {
     let file = File::open(xml_file)?;
     let parser = EventReader::new(file);
-    let mut subtitles: Vec<String> = vec![];
+    // let mut subtitles = Vec::with_capacity(100); // Preallocate with initial capacity
+    let mut subtitles = Vec::new();
+    let mut buffer = String::new(); // Use a buffer to avoid allocating new strings
     for event in parser.into_iter() {
         match event {
+            // NOTE: Don't need this now.
+            // Ok(XmlEvent::StartElement { name, attributes, .. }) => {
+            //     get_timestamps(name, attributes, &mut subtitles) }
             Ok(XmlEvent::Characters(s)) => {
                 if !s.trim().is_empty() {
-                    subtitles.push(s);
+                    buffer.push_str(&s);
                 }
             }
-            /* Ok(XmlEvent::StartElement {
-                name, attributes, ..
-            }) => {
-                if name.local_name == "p" {
-                    let mut text = String::new();
-                    attributes
-                        .iter()
-                        .for_each(|attr| match attr.name.local_name.as_str() {
-                            "begin" => text += &format!("{} - ", attr.value),
-                            "end" => text += &format!("{}\n", attr.value),
-                            _ => {}
-                        });
-                    // NOTE: Don't need this now.
-                    // subtitles.push(text)
-                }
-            } */
             _ => {}
+        }
+        if !buffer.trim().is_empty() {
+            subtitles.push(buffer.clone());
+            buffer.clear();
         }
     }
     Ok(subtitles)
+}
+
+#[allow(unused)]
+fn get_timestamps(
+    name: xml::name::OwnedName,
+    attributes: Vec<xml::attribute::OwnedAttribute>,
+    subtitles: &mut Vec<String>,
+) {
+    if name.local_name == "p" {
+        let mut text = String::new();
+        attributes.iter().for_each(|attr| match attr.name.local_name.as_str() {
+            "begin" => text += &format!("{} - ", attr.value),
+            "end" => text += &format!("{}\n", attr.value),
+            _ => {}
+        });
+        // NOTE: Don't need this now.
+        subtitles.push(text)
+    }
 }
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -101,6 +125,7 @@ fn csv_write_subtitles(path: &str, subtitles_global: &[String]) -> anyhow::Resul
 
     // Write header row. Write subtitle rows.
     wtr.write_record(["subtitle"])?;
+    // subtitles_global.par_iter().for_each(|subtitle| wtr.write_record([subtitle]).unwrap());
     for subtitle in subtitles_global.iter() {
         wtr.write_record([subtitle]).unwrap()
     }
