5428bfb8c19dbb31f90ad4e7457fbb0aa5fb4208
Lloyd Lobo <lloydlobo@gmail.com>
2023-02-17 08:18:54 +0530

    refactor: move rust code to workspace with xtask crates

---
+++ crates/ytscriptrs/src/main.rs
@@ -0,0 +1,23 @@
+use std::process::Output;
+
+use ytscriptrs::*;
+
+//////////////////////////////////////////////////////////////////////////////////
+
+fn main() {
+    try_main().unwrap();
+}
+
+fn try_main() -> anyhow::Result<()> {
+    let output: Output = download_youtube_subs(*URL)?;
+    let file_api_video_title: &str = &String::from_utf8(output.stdout)?;
+    let xml_file_path = find_subtitle_filename_no_shrink(file_api_video_title)?;
+    let subtitles_global = extract_subtitles_xml(&xml_file_path)?;
+    // let output_csv = format!("sub_{filename}.csv", filename = xml_file_path);
+    let output_csv = "data.csv".to_string();
+    csv_write_subtitles(&output_csv, &subtitles_global)?;
+
+    Ok(())
+}
+
+//////////////////////////////////////////////////////////////////////////////////
